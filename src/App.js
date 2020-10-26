61629bb5e8c8af0627a5a9306331a183327bce15
lucasdgimenez <lucas_d360@hotmail.com>
2020-10-26 11:48:04 -0300

    tres

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-import { firebaseAuth } from "./provider/AuthProvider";
+//import { firebaseAuth } from "./provider/AuthProvider";
 import Routes from "./Routes";
 import TheHeader from "./components/TheHeader"
 import TheFooter from "./components/TheFooter"
 
 function App() {
-  const { handleSignup } = useContext(firebaseAuth);
+  //const { handleSignup } = useContext(firebaseAuth);
 
-  const { test } = useContext(firebaseAuth);
+  //const { test } = useContext(firebaseAuth);
   return (
     <>
       <BrowserRouter>
